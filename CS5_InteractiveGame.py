11f275a71b23b1322923df77ec40b8587d0eaf39
TruestGecko <92557966+TruestGecko@users.noreply.github.com>
2023-09-22 20:33:56 -0500

    Maybe finished interactive game

--- CS5_InteractiveGame.py
+++ CS5_InteractiveGame.py
@@ -1,30 +1,48 @@
 import random
 import time
+import os
 
 class enemy:
     def __init__(self, name, inputMinAtk,inputMaxAtk):
         self.name = name
         self.minAtk = inputMinAtk
         self.maxAtk = inputMaxAtk
+
 atkMin = 1
 atkMax = 6
 
-deathByRabbit  = "rabbit"
-deathByLion = "lion"
-deathByHidingFromAuthorities1 = ""
-deathByHidingInCave = ""
+deathByHidingInCave = [" You stay in the cave. \n\n\n\n\n\n", " You are still in the cave. \n\n\n\n\n\n", " Why are you here again? \n\n\n\n\n\n", "Are you ever going to leave? \n\n\n\n\n\n\n"]
+deathByRabbit = [" You were obliterated by a rabbit...\n ... somehow."]
+deathByHidingFromAuthorities1 = [" You decided to hide from the authorities.", " First, you went into the woods and built a house.", " You lived there for 71 years, without seeing anyone else.", " Then, without warning, a team of 7 authorities appeared in your house while you were eating lunch and apprehended you."]
+deathByHidingFromAuthorities2 = [" You decided to hide from the authorities.", " First, you went into the woods and built a house.", " You lived there for 71 years, without seeing anyone else.", " Then, the ground starts moving.", " The Lion appears behind you and obliterates you, your house, and the entire earth."]
+deathByLion = [" You were obliterated by The Lion in one attack.", " In just three days, the rest of the world have been destroyed by The Lion's claws."]
 rabbit = enemy("rabbit", 0,3)
 lion = enemy("lion", 3, 8)
 
 def wait(seconds):
     time.sleep(seconds)
 
+def choose(option1, option2):
+    wait(0.75)
+    choice = input("Choose '" + option1 + "' or '" + option2 + "' " )
+    wait(0.75)
+    while choice != option1 and choice != option2:
+        print("That isn't a valid choice. Please choose again")
+        wait(0.75)
+        choice = input("Choose '" + option1 + "' or '" + option2 + "' " )
+        wait(0.75)
+    return choice == option1
+
 def win():
+    print("\n The lion approaches.\n You are scared \n The lion attacks.\n You step to the side. \n The lion falls to the ground. \n You attack.\n The lion can't dodge it.")
     print("You won")
+    print(":D")
 
 def lose(reason):
-    print(reason)
-    wait(0.75)
+    print("\n")
+    for line in reason:
+        print(line)
+        wait(0.75)
     print("You lose!")
     wait(0.75)
     print(":(")
@@ -35,13 +53,13 @@ def rollTwoDice(min, max):
 def combat(enemy):
     yourAttack = rollTwoDice(atkMin, atkMax)
     enemyAttack = rollTwoDice(enemy.minAtk, enemy.maxAtk)
-    print("You entered a battle!!")
+    print("\nYou entered a battle!!")
     wait(0.75)
     print("You're fighting a " + enemy.name)
     wait(0.75)
     print("You rolled a " + str(yourAttack) + "!")
     wait(0.75)
-    print("Your opponent rolled a " + str(enemyAttack))
+    print("The", enemy.name, "rolled a " + str(enemyAttack))
     wait(0.75)
     if (enemyAttack > yourAttack):
         print("You lost the fight!")
@@ -60,46 +78,84 @@ def combat(enemy):
 
 def increaseStats():
     global atkMax
-    print("Your max attack increased by 1!")
+    print("\nYour max attack increased by 2!")
     wait(0.75)
     atkMax += 1
-    print("It's now", atkMax)
+    print("It's now", atkMax * 2)
     wait(0.75)
-    choice = input("Do you want to keep fighting rabbits? Or do you want to try and fight the lion? \nChoose 'rabbit' or 'lion' ")
-    if choice == "rabbit":
+    print("Do you want to keep fighting rabits, or do you want to try and fight the lion?")
+    if choose("rabbit", "lion"):
         combat(rabbit)
     else:
         combat(lion)
 
-def field():
-    print("field")
+def cave():
+    print("\nYou enter the cave...")
+    wait(0.75)
+    print("Suddenly you hear a noise. When you look over, you see a rabbit, glaring at you")
+    wait(0.75)
+    print("Will you fight the rabbit, or hide in the cave?")
+    if choose("fight", "hide"):
+        combat(rabbit)
+    else:
+        lose(deathByHidingInCave)
 
+def field():
+    print("\nWhen you open your eyes, you find yourself stand in an empty field...")
+    wait(0.75)
+    print("But then you hear a loud roar. Turning around, you come face to face with a lion!")
+    wait(0.75)
+    print("You also spot a small opening to a cave that the lion couldn't fit in")
+    wait(0.75)
+    print("Will you try to fight the lion, or run into the cave?")
+    if choose("fight", "run"):
+        combat(lion)
+    else:
+        cave()
+
+def takeTrain():
+    print("\nYou board the train...")
+    wait(0.75)
+    print("Conductor: Welcome aboard all you brave heroes who are going to fight the mighty lion!")
+    wait(0.75)
+    print("Will you stay on the train, or get off and try to hide from the government?")
+    if choose("fight", "hide"):
+        field()
+    else:
+        lose(random.choice(deathByHidingFromAuthorities1, deathByHidingFromAuthorities2))
+    
 def talkToMan():
-    print("You go over to talk to the man")
+    print("\nYou go over to talk to the man")
     wait(0.75)
     print("He says: All people over the age of -1 are being recruited to fight the mighty lion terroizing the earth")  
     wait(0.75)
-    print("Will you go to help stop the lion, or live a life on the run from the authorities")
-    wait(0.75)
-    choice = input("Choose 'fight' or 'hide'")
-    if choice == "fight":
+    print("Will you go to help stop the lion, or live a life on the run from the authorities?")
+    if choose("fight", "hide"):
         takeTrain()
     else: 
-        lose(deathByHidingFromAuthorities1)
-def city():
-    print("city")
+        lose(random.choice([deathByHidingFromAuthorities1, deathByHidingFromAuthorities2]))
 
+def city():
+    print("\nWelcome to the city!")
+    wait(0.75)
+    print("You see a old man waving his hand at you, but then behind you, you hear:")
+    wait(0.75)
+    print("Last call for the train!!")
+    wait(0.75)
+    print("Will you go talk to the old man or board the train?")
+    if choose("talk", "train"):
+        talkToMan()
+    else:
+        takeTrain()
 
 def start():
+    os.system('cls') # Clears the console
     print("Welcome player!")
     wait(0.75)
-    print("Do you want to start in the fields or the city")
-    wait(0.75)
-    choice = input("Choose 'field' or 'city'")
-    wait(0.75)
-    if choice == "field":
+    print("Do you want to start in the fields or the city?")
+    if choose("field", "city"):
         field()
     else:
         city()
 
-
+start()
