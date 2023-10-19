859c6d9685008743d90fb9d748a3fadab8bf507b
TruestGecko <92557966+VimalBuckley@users.noreply.github.com>
2023-10-19 18:49:14 -0500

    Even Better Whack A Mole

--- CS11_WhackAMole.py
+++ CS11_WhackAMole.py
@@ -1,6 +1,7 @@
 import turtle
 import random
 import os
+from operator import itemgetter
 
 global xClickThreshold
 global yClickThreshold
@@ -45,9 +46,12 @@ def setup(numberOfTurtles):
     randomizeTurtleLocations()
 
 def randomizeTurtleLocations():
+    theseLocations = [(-200, 150), (0, 150), (200, 150), (-200, 0), (0,0), (200, 0), (-200, -150), (0, -150), (200, -150)]
     for t in turtles:
         t2: turtle = t
-        t2.goto(random.choice(locations))
+        thisLocation = random.choice(theseLocations)
+        theseLocations.remove(thisLocation)
+        t2.goto(thisLocation)
         t2.showturtle()
 
 def clock():
@@ -74,21 +78,53 @@ def clicked(x,y):
             scoringTurtle.write("Score: " + str(playerScore), font = ("Arial", 20, "normal"))
 
 def setDown():
-    # for t in turtles:
-    #     t2: turtle = t
-    #     t2.hideturtle()
-    #     t2.goto(1000,1000)
-    # timerTurtle.clear()
-    # timerTurtle.home()
-    # timerTurtle.write("Game Over", font = ("Arial", 20, "normal"))
     print("Your score was: " + str(playerScore))
     s.bye()
+
+def readScores(fileName: str = "CS11_Highscores.txt"):
+    fileReader = open(fileName, "r")
+    nameScoreList = []
+    for line in fileReader:
+        nameScoreList.append((line.split(": ")))
+    fileReader.close()
+    for pairing in nameScoreList:
+        pairing[1] = int(pairing[1])
+    return nameScoreList
+
+def sortScores(nameScoreList: list):
+    nameScoreList.sort(key = lambda x: x[1], reverse = True)
+    return nameScoreList
+
+def updateScores(playerScore: int, oldNameScoreList: list):
+    playerName = input("What's your name? ")
+    nameScoreList = sortScores(oldNameScoreList)
+    playerPair = (playerName, playerScore)
+    nameScoreList.append(playerPair)
+    for pair in nameScoreList:
+        if playerPair[0] == pair[0] and not playerPair == pair:
+            if playerPair[1] > pair[1]:
+                nameScoreList.remove(pair)
+            else:
+                nameScoreList.remove(playerPair)
+            break
+    nameScoreList = sortScores(nameScoreList)
+    while len(nameScoreList) > 5:
+        nameScoreList.pop()
+    return nameScoreList
+
+def writeScores(nameScoreList: list, fileName: str = "CS11_Highscores.txt"):
+    fileWriter = open(fileName, "w")
+    for pair in nameScoreList:
+        fileWriter.write(pair[0] + ": " + str(pair[1]) + "\n")
+    fileWriter.close()
+
 
 def dealWithScoring():
     global playerName
     global playerScore
     nameList = []
     scoreList = []
+    nameAndScoreList = []
     newNameList = []
     newScoreList = []
     foundPlace = False
@@ -99,7 +135,12 @@ def dealWithScoring():
         score = score.split(" ")[0]
         nameList.append(name)
         scoreList.append(score)
+        nameAndScoreList.append((name, score))
     info.close()
+    foundMatch = False
+    for nameScorePair in nameAndScoreList:
+        if nameScorePair[0] == playerName:
+            foundMatch = True
     for i in range(len(scoreList)):
         if int(scoreList[i]) < int(playerScore) and not foundPlace:
             newNameList.append(playerName)
@@ -116,9 +157,8 @@ def dealWithScoring():
         file.write(newNameList[i] + "," + str(newScoreList[i]) + " \n")
     file.close()
 
-
 setup(4)
 clock()
 s.onclick(clicked)
 s.mainloop()
-dealWithScoring()
+writeScores(updateScores(playerScore, readScores()))
