5e4c8842fddfd2d1b6dec98d428ab1a53514f43c
VimalBuckley <92557966+VimalBuckley@users.noreply.github.com>
2023-10-31 12:08:19 -0500

    More work

--- CS12_CustomGame.py
+++ CS12_CustomGame.py
@@ -5,25 +5,33 @@ os.system("cls")
 myScreen = turtle.Screen()
 myScreen.screensize(500, 500)
 setupTurtle = turtle.Turtle()
-setupTurtle.hideturtle()
-setupTurtle.speed(0)
+setupTurtle.showturtle()
+setupTurtle.speed(9)
 setupTurtle.penup()
 setupTurtle.goto(0, -250)
-setupTurtle.color("red")
-setupTurtle.pendown()
-setupTurtle.begin_fill()
-setupTurtle.circle(250)
+# setupTurtle.color("red")
+# setupTurtle.pendown()
+# setupTurtle.begin_fill()
+# setupTurtle.circle(250)
 setupTurtle.end_fill()
 print(myScreen.screensize())
 
 class clickTarget():
-    def __init__(self, coordinate: tuple = (0,0), duration :float = 3):
-        self.coordinate = coordinate
+    def __init__(self, angles: tuple = (0,0), duration :float = 3):
+        self.angles = angles
         self.duration = duration
-        self.turtle1 = turtle.Turtle()
-        self.turtle2 = turtle.Turtle()
-    
+        self.turtle1 = turtle.Turtle(False)
+        self.turtle1.hideturtle()
+        self.turtle1.speed(0)
+        self.turtle1.penup()
 
+        self.turtle2 = turtle.Turtle(False)
+        self.turtle2.hideturtle()
+        self.turtle2.speed(0)
+        self.turtle2.penup()
+    
+    def loadTurtles(self):
+        self.turtle1.goto()
         
 
 
