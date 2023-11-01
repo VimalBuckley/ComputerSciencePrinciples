590080d87e7f8f6e63f6215c3484acbcb793ac5f
TruestGecko <92557966+VimalBuckley@users.noreply.github.com>
2023-11-01 18:57:02 -0500

    Finished Framework

--- CS12_CustomGame.py
+++ CS12_CustomGame.py
@@ -1,75 +1,88 @@
 import turtle
 import math
 import random
+from enum import Enum
 import os
 os.system("cls")
+upKey = input("Choose your up keybind\n")
+leftKey = input("Choose your left keybind\n")
+downKey = input("Choose your down keybind\n")
+rightKey = input("Choose your right keybind\n")
 myScreen = turtle.Screen()
 myScreen.screensize(500, 500)
-setupTurtle = turtle.Turtle()
-setupTurtle.hideturtle()
-setupTurtle.speed(0)
-setupTurtle.penup()
-setupTurtle.goto(0, -250)
-setupTurtle.color("red")
-setupTurtle.pendown()
-setupTurtle.begin_fill()
-setupTurtle.circle(250)
-setupTurtle.end_fill()
-print(myScreen.screensize())
+turtles = []
+keysPressed = {"test"}
+keysPressed.discard("test")
 
-class clickTarget():
-    def __init__(self):
-        self.locations = [(250, 0, -180), (0, 250, 270), (-250, 0, 0), (0, -250, 90)]
-        self.turtle1 = turtle.Turtle()
-        self.turtle1.hideturtle()
-        self.turtle1.speed(0)
-        self.turtle1.penup()
+class Lane(Enum):
+    RIGHT = ((400, 0), 180)
+    UP = ((0, 400), 270)
+    LEFT = ((-400, 0), 0)
+    DOWN = ((0, -400), 90)
 
-        self.turtle2 = turtle.Turtle()
-        self.turtle2.hideturtle()
-        self.turtle2.speed(0)
-        self.turtle2.penup()
+class NoteReqs():
+    def __init__(self, lane: Lane, timeMilliseconds: int):
+        self.lane = lane
+        self.time = timeMilliseconds
+
+class CustomTurtle():
+    def __init__(self, lane: Lane):
+        self.lane = lane
+        self.myTurtle = turtle.Turtle()
+        self.myTurtle.hideturtle()
+        self.myTurtle.speed(0)
+        self.myTurtle.penup()
+        self.myTurtle.goto(self.lane.value[0])
+        self.myTurtle.setheading(self.lane.value[1])
+        self.myTurtle.shape("square")
+        self.myTurtle.color("red")
+        self.myTurtle.showturtle()
+
+    def moveForward(self):
+        self.myTurtle.forward(5)
+
+    def distanceFromStart(self):
+        return math.dist(self.lane.value[0], self.myTurtle.pos())
     
-    def loadTurtles(self):
-        self.turtle1.hideturtle()
-        self.turtle2.hideturtle()
-        locations = [(250, 0, -180), (0, 250, 270), (-250, 0, 0), (0, -250, 90)]
-        choice = random.choice(locations)
-        locations.remove(choice)
-        self.turtle1.goto(choice[0], choice[1])
-        self.turtle1.setheading(choice[2])
-        choice2 = random.choice(locations)
-        self.turtle2.goto(choice2[0], choice2[1])
-        self.turtle2.setheading(choice2[2])
-        self.turtle1.showturtle()
-        self.turtle2.showturtle()
-        locations.append(choice)
+    def isDone(self):
+        if self.distanceFromStart() > 400:
+            self.end()
+            return True
+        return False
 
-    def moveTurtles(self):
-        self.turtle1.forward(5)
-        self.turtle2.forward(5)
-        myScreen.ontimer(self.moveTurtles, 5)
-        
-origin = (0,0)
-globalTarget = clickTarget()
-globalTarget.loadTurtles()
-globalTarget.moveTurtles()
+    def end(self):
+        self.myTurtle.hideturtle()
 
-def makeTarget():
-    globalTarget.loadTurtles()
-    globalTarget.moveTurtles()
+def moveTurtles():
+    for t in turtles:
+        t2: CustomTurtle = t
+        t2.moveForward()
+        if (t2.isDone()):
+            turtles.pop(0)
 
-def checkForClick(x, y):
-    clickNearOrigin = math.dist(origin, (x,y)) < 100
-    turtlesNearOrigin = math.dist(origin, globalTarget.turtle1.pos()) < 100
-    if clickNearOrigin and turtlesNearOrigin:
-        print("clicked")
-        del globalTarget
-        globalTarget = clickTarget()
-        makeTarget()
-    else:
-        print("missed")
+def gameLoop():
+    moveTurtles()
+    myScreen.ontimer(gameLoop, 1)
 
+def testPrint():
+    print("Hi")
+
+def createNotes(notes: list = [NoteReqs(Lane.RIGHT, 0)]):
+    for possibleNote in notes:
+        createNote(possibleNote)
+
+def createNote(note):
+    myScreen.ontimer(lambda : turtles.append(CustomTurtle(note.lane)), note.time)
+
+createNotes([])
+gameLoop()
 myScreen.listen()
-myScreen.onclick(checkForClick)
+myScreen.onkeypress(lambda : keysPressed.add(Lane.UP), upKey)
+myScreen.onkeypress(lambda : keysPressed.add(Lane.DOWN), downKey)
+myScreen.onkeypress(lambda : keysPressed.add(Lane.LEFT), leftKey)
+myScreen.onkeypress(lambda : keysPressed.add(Lane.RIGHT), rightKey)
+myScreen.onkey(lambda : keysPressed.discard(Lane.UP), upKey)
+myScreen.onkey(lambda : keysPressed.discard(Lane.DOWN), downKey)
+myScreen.onkey(lambda : keysPressed.discard(Lane.LEFT), leftKey)
+myScreen.onkey(lambda : keysPressed.discard(Lane.RIGHT), rightKey)
 myScreen.mainloop()
