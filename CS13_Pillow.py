d1ec36766c1c818242949124558809e136e52412
VimalBuckley <92557966+VimalBuckley@users.noreply.github.com>
2023-12-05 11:01:17 -0600

    Text on image

--- CS13_Pillow.py
+++ CS13_Pillow.py
@@ -1,8 +1,10 @@
-from PIL import Image
+from PIL import Image, ImageChops, ImageDraw
 
 earth = Image.open("CS13_Earth.png")
 dog = Image.open("CS13_GoldenRetriever.jpg")
 smaller_earth = earth.resize((30, 30))
 dog.paste(smaller_earth, (60, 50), mask=smaller_earth)
 dog.paste(smaller_earth, (120, 55), mask=smaller_earth)
+drawingDow = ImageDraw.Draw(dog)
+drawingDow.text((50,200), "This is a dog!!", font_size=20)
 dog.show()
