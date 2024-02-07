e3ec6a5c71dc0d4b6383948b5d69a758f8231b39
VimalBuckley <92557966+VimalBuckley@users.noreply.github.com>
2024-02-07 21:02:44 +0000

    Used List Comprehension and Caught Errors

--- Encryption/CS17_CeaserCypher.py
+++ Encryption/CS17_CeaserCypher.py
@@ -1,24 +1,23 @@
-charcters: list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", "'"]
-
 def encrypt(message: str, shift: int):
-    encryptedCharacters = []
-    for letter in message:
-        encryptedCharacters.append(charcters[loopIndex(charcters.index(letter), shift)])
-    return "".join(encryptedCharacters)
+    characters: list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", "'"]
+    badCharacters = [letter for letter in message if letter not in characters]
+    if len(badCharacters) != 0:
+        raise Exception("The messaged contained unrecognized characters:", badCharacters)
+    return "".join(characters[loopIndex(characters.index(letter), shift, len(characters))] for letter in message)
 
 def decrypt(encrptedMessage: str, shift: int):
     return encrypt(encrptedMessage, -shift)
     
-def loopIndex(inital, shift):
+def loopIndex(inital: int, shift: int, length: int):
     if inital < 0:
         inital = 0
-    if (inital > len(charcters) - 1):
-        inital = len(charcters) - 1
+    if (inital > length - 1):
+        inital = length - 1
     sum = inital + shift
-    while (sum > len(charcters) - 1):
-        sum -= (len(charcters)) 
+    while (sum > length - 1):
+        sum -= length 
     while (sum < 0):
-        sum += (len(charcters))
+        sum += length
     return sum
 
 print(encrypt("flajsf4793", 2))
