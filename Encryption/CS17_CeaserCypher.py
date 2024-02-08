d4f617cb28404fe8590d3d2fd4c5c0f492f6ddfe
VimalBuckley <92557966+VimalBuckley@users.noreply.github.com>
2024-02-07 21:20:48 -0600

    Added a way to brute force

--- Encryption/CS17_CeaserCypher.py
+++ Encryption/CS17_CeaserCypher.py
@@ -1,12 +1,17 @@
+characters: list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", "'"]
+
 def encrypt(message: str, shift: int):
-    characters: list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", "'"]
     badCharacters = [letter for letter in message if letter not in characters]
     if len(badCharacters) != 0:
         raise Exception("The messaged contained unrecognized characters:", badCharacters)
     return "".join(characters[loopIndex(characters.index(letter), shift, len(characters))] for letter in message)
 
-def decrypt(encrptedMessage: str, shift: int):
-    return encrypt(encrptedMessage, -shift)
+def decrypt(encryptedMessage: str, shift: int):
+    return encrypt(encryptedMessage, -shift)
+
+def bruteForce(encryptedMessage: str):
+    for index in range(len(characters)):
+        print(decrypt(encryptedMessage, index))
     
 def loopIndex(inital: int, shift: int, length: int):
     if inital < 0:
@@ -20,5 +25,8 @@ def loopIndex(inital: int, shift: int, length: int):
         sum += length
     return sum
 
-print(encrypt("flajsf4793", 2))
-print(decrypt(encrypt("flajsf4793", 2), 2))
+shift = 156
+encrypted = encrypt("My name is Vimal!", shift)
+decrypted = decrypt(encrypted, shift)
+print(encrypted)
+print(decrypted)
