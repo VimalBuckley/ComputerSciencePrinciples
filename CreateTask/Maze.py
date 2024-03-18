cbc97fbf0731e0b2e8dd29f2aff6c47766e332e1
VimalBuckley <92557966+VimalBuckley@users.noreply.github.com>
2024-03-18 16:03:06 -0500

    Everything Works!
    Going to add a few more things:
    - Player Name
    - Maybe Leaderboard?
    - Making Maze Creation More Idiot Proof

--- CreateTask/Maze.py
+++ CreateTask/Maze.py
@@ -1,19 +1,28 @@
 import turtle
+from enum import Enum
+import time
+
+class Tile(Enum):
+    SPACE = (False, "white")
+    WALLL = (True, "black")
+    ENDDD = (False, "red")
+    START = (False, "green")
+
 maze = [
-    [False, False, False, False, False, False, False],
-    [False, False, True , True , True , False, False], 
-    [False, True , False, False, False, True , False], 
-    [False, True , False, False, False, True , False], 
-    [False, True , False, False, False, True , False], 
-    [False, False, True , True , True , False, False],
-    [False, False, False, False, False, False, False]
+    [Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE],
+    [Tile.SPACE, Tile.SPACE, Tile.ENDDD, Tile.WALLL, Tile.WALLL, Tile.SPACE, Tile.SPACE], 
+    [Tile.SPACE, Tile.WALLL, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.WALLL, Tile.SPACE], 
+    [Tile.SPACE, Tile.WALLL, Tile.SPACE, Tile.START, Tile.SPACE, Tile.WALLL, Tile.SPACE], 
+    [Tile.SPACE, Tile.WALLL, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.WALLL, Tile.SPACE], 
+    [Tile.SPACE, Tile.SPACE, Tile.WALLL, Tile.WALLL, Tile.WALLL, Tile.SPACE, Tile.SPACE],
+    [Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE]
 ]
 
 class Wall:
     def __init__(self, row: int, column: int):
         self.row = row
         self.column = column
-        self.turt = turtle.Turtle(visible=False)
+        self.turt = turtle.Turtle()
         self.turt.penup()
         self.turt.speed(0)
         self.turt.shape("square")
@@ -23,39 +32,64 @@ class Wall:
         self.turt.showturtle()
     
     def check(self):
-        if (maze[playerRow + self.row][playerColumn + self.column]):
-            self.turt.showturtle()
-        else:
-            self.turt.hideturtle()
-
-
+        try:
+            self.turt.color(maze[playerRow + self.row][playerColumn + self.column].value[1])
+        except:
+            self.turt.color("white")
 screen = turtle.Screen()
 screen.tracer(0, 0)
 screen.setup(600, 600)
-player = turtle.Turtle(visible=False)
+player = turtle.Turtle()
 player.shape("square")
 player.shapesize(6, 6, 1)
-player.color("red")
+player.color("blue")
 player.penup()
 player.showturtle()
-playerColumn = 3
-playerRow = 3
+playerColumn = 0
+playerRow = 0
+startFound = False
+for rowIndex in range(len(maze)):
+    for columnIndex in range(len(maze[rowIndex])):
+        if maze[rowIndex][columnIndex] == Tile.START:
+            playerColumn = columnIndex
+            playerRow = rowIndex
+            if startFound == True:
+                raise Exception("Multiple starts were found in the maze. Please make sure to provide exactly one start tile!")
+            startFound = True
+if not startFound:
+    raise Exception("The start of the maze coulnd't be found. Please make sure a start tile was provided!")
+startTime = 0
+
 walls = [Wall(row, column) for row in range(-2, 3) for column in range(-2, 3) if not (row == 0 and column == 0)]
+def end():
+    print("You won!")
+    print("Your time was:", str(time.time() - startTime), "seconds!")
 def move(deltaRow: int, deltaColumn: int):
     global playerRow
     global playerColumn
-    playerRow += deltaRow
-    playerColumn += deltaColumn
-    if (maze[playerRow][playerColumn]):
-        playerRow -= deltaRow
-        playerColumn -= deltaColumn
-    for wall in walls:
-        wall.check()
-    screen.update()
+    nextTile = maze[playerRow + deltaRow][playerColumn + deltaColumn]
+    if nextTile == Tile.WALLL:
+        pass
+    elif nextTile == Tile.SPACE or nextTile == Tile.START:
+        playerRow += deltaRow
+        playerColumn += deltaColumn
+        for wall in walls:
+            wall.check()
+        screen.update()
+    elif nextTile == Tile.ENDDD:
+        playerRow += deltaRow
+        playerColumn += deltaColumn
+        screen.bye()
+        end()
 screen.onkeypress(lambda: move(-1, 0), "w")
 screen.onkeypress(lambda: move(1, 0), "s")
 screen.onkeypress(lambda: move(0, -1), "a")
 screen.onkeypress(lambda: move(0, 1), "d")
+screen.onkeypress(lambda: move(-1, 0), "Up")
+screen.onkeypress(lambda: move(1, 0), "Down")
+screen.onkeypress(lambda: move(0, -1), "Left")
+screen.onkeypress(lambda: move(0, 1), "Right")
 screen.listen()
+startTime = time.time()
 move(0, 0)
 screen.mainloop()
