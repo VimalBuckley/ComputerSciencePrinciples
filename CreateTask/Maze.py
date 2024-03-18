34879034ceae88094eb3b4824dd935cdd36bd0a5
VimalBuckley <92557966+VimalBuckley@users.noreply.github.com>
2024-03-18 16:40:50 -0500

    Lots of Organizing
    Made maze checking better, and organized into functions

--- CreateTask/Maze.py
+++ CreateTask/Maze.py
@@ -2,6 +2,14 @@ import turtle
 from enum import Enum
 import time
 
+screen = turtle.Screen()
+player = turtle.Turtle()
+playerColumn = 0
+playerRow = 0
+mazeHeight = 0
+mazeWidth = 0
+startTime = 0
+walls = []
 class Tile(Enum):
     SPACE = (False, "white")
     WALLL = (True, "black")
@@ -14,7 +22,7 @@ maze = [
     [Tile.SPACE, Tile.WALLL, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.WALLL, Tile.SPACE], 
     [Tile.SPACE, Tile.WALLL, Tile.SPACE, Tile.START, Tile.SPACE, Tile.WALLL, Tile.SPACE], 
     [Tile.SPACE, Tile.WALLL, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.WALLL, Tile.SPACE], 
-    [Tile.SPACE, Tile.SPACE, Tile.WALLL, Tile.WALLL, Tile.WALLL, Tile.SPACE, Tile.SPACE],
+    [Tile.SPACE, Tile.SPACE, Tile.WALLL, Tile.SPACE, Tile.WALLL, Tile.SPACE, Tile.SPACE],
     [Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE, Tile.SPACE]
 ]
 
@@ -31,65 +39,99 @@ class Wall:
         self.turt.goto(column * 120, -row * 120)
         self.turt.showturtle()
     
-    def check(self):
-        try:
+    def updateColor(self):
+        # if not (
+        #     playerRow + self.row < 0 or playerRow + self.row > mazeHeight or
+        #     playerColumn + self.column < 0 or playerColumn + self.column > mazeWidth
+        # ):
+        if checkInbounds(playerRow + self.row, playerColumn + self.column):
             self.turt.color(maze[playerRow + self.row][playerColumn + self.column].value[1])
-        except:
-            self.turt.color("white")
-screen = turtle.Screen()
-screen.tracer(0, 0)
-screen.setup(600, 600)
-player = turtle.Turtle()
-player.shape("square")
-player.shapesize(6, 6, 1)
-player.color("blue")
-player.penup()
-player.showturtle()
-playerColumn = 0
-playerRow = 0
-startFound = False
-for rowIndex in range(len(maze)):
-    for columnIndex in range(len(maze[rowIndex])):
-        if maze[rowIndex][columnIndex] == Tile.START:
-            playerColumn = columnIndex
-            playerRow = rowIndex
-            if startFound == True:
-                raise Exception("Multiple starts were found in the maze. Please make sure to provide exactly one start tile!")
-            startFound = True
-if not startFound:
-    raise Exception("The start of the maze coulnd't be found. Please make sure a start tile was provided!")
-startTime = 0
+        else:
+            self.turt.color("black")
 
-walls = [Wall(row, column) for row in range(-2, 3) for column in range(-2, 3) if not (row == 0 and column == 0)]
+def startGame():
+    global walls
+    global startTime
+    checkMaze()
+    screen.tracer(0, 0)
+    screen.setup(600, 600)
+    player.shape("square")
+    player.shapesize(6, 6, 1)
+    player.color("blue")
+    player.penup()
+    player.showturtle()
+    walls = [Wall(row, column) for row in range(-2, 3) for column in range(-2, 3) if not (row == 0 and column == 0)]
+    screen.onkeypress(lambda: move(-1, 0), "w")
+    screen.onkeypress(lambda: move(1, 0), "s")
+    screen.onkeypress(lambda: move(0, -1), "a")
+    screen.onkeypress(lambda: move(0, 1), "d")
+    screen.onkeypress(lambda: move(-1, 0), "Up")
+    screen.onkeypress(lambda: move(1, 0), "Down")
+    screen.onkeypress(lambda: move(0, -1), "Left")
+    screen.onkeypress(lambda: move(0, 1), "Right")
+    screen.listen()
+    move(0, 0)
+    startTime = time.time()
+    screen.mainloop()
+
+def checkMaze():
+    '''
+    Makes sure that the maze fits all the criteria of being a maze, to minimize bugs during gameplay\n
+    These include:\n
+    \t- All rows are the same length\n
+    \t- There is exactly one start tile\n
+    \t- There is at least one end tile\n
+    '''
+    
+    global playerColumn
+    global playerRow
+    global mazeHeight
+    global mazeWidth
+    startFound = False
+    endFound = False
+    mazeHeight = len(maze) - 1
+    for rowIndex in range(len(maze)):
+        if rowIndex == 0:
+            mazeWidth = len(maze[rowIndex]) - 1
+        elif mazeWidth != len(maze[rowIndex]) - 1:
+            raise Exception("Rows of differing length were found. Please note that all rows must be the same length!")
+        for columnIndex in range(len(maze[rowIndex])):
+            if maze[rowIndex][columnIndex] == Tile.START:
+                if startFound:
+                    raise Exception("Multiple start tiles were found. Please include exactly one start tile!")
+                else:
+                    playerColumn = columnIndex
+                    playerRow = rowIndex
+                    startFound = True
+            elif maze[rowIndex][columnIndex] == Tile.ENDDD:
+                endFound = True
+    if not endFound:
+        raise Exception("No end tiles were found. Please include at least one end tile!")
+
+def checkInbounds(row: int, column: int):
+    return not(row < 0 or row > mazeHeight or column < 0 or column > mazeWidth)
+
 def end():
     print("You won!")
     print("Your time was:", str(time.time() - startTime), "seconds!")
+
 def move(deltaRow: int, deltaColumn: int):
     global playerRow
     global playerColumn
-    nextTile = maze[playerRow + deltaRow][playerColumn + deltaColumn]
-    if nextTile == Tile.WALLL:
-        pass
-    elif nextTile == Tile.SPACE or nextTile == Tile.START:
-        playerRow += deltaRow
-        playerColumn += deltaColumn
-        for wall in walls:
-            wall.check()
-        screen.update()
-    elif nextTile == Tile.ENDDD:
-        playerRow += deltaRow
-        playerColumn += deltaColumn
-        screen.bye()
-        end()
-screen.onkeypress(lambda: move(-1, 0), "w")
-screen.onkeypress(lambda: move(1, 0), "s")
-screen.onkeypress(lambda: move(0, -1), "a")
-screen.onkeypress(lambda: move(0, 1), "d")
-screen.onkeypress(lambda: move(-1, 0), "Up")
-screen.onkeypress(lambda: move(1, 0), "Down")
-screen.onkeypress(lambda: move(0, -1), "Left")
-screen.onkeypress(lambda: move(0, 1), "Right")
-screen.listen()
-startTime = time.time()
-move(0, 0)
-screen.mainloop()
+    if checkInbounds(playerRow + deltaRow, playerColumn + deltaColumn):
+        nextTile = maze[playerRow + deltaRow][playerColumn + deltaColumn]
+        if nextTile == Tile.WALLL:
+            pass
+        elif nextTile == Tile.SPACE or nextTile == Tile.START:
+            playerRow += deltaRow
+            playerColumn += deltaColumn
+            for wall in walls:
+                wall.updateColor()
+            screen.update()
+        elif nextTile == Tile.ENDDD:
+            playerRow += deltaRow
+            playerColumn += deltaColumn
+            screen.bye()
+            end()
+
+startGame()
